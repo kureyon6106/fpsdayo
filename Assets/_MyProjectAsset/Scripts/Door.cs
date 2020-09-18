91b2b4e2a5ccd04e453a5000541f256c0729882a
kureyon6106 <kureyon6106@i.softbank.jp>
2020-09-18 20:12:04 +0900

    add MyProject data

---
+++ Assets/_MyProjectAsset/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private Animator animator;
+    private bool isOpen;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = animator.GetBool("isOpen");
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+        if (other.tag != "Player") return;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isOpen = !isOpen;
+            animator.SetBool("isOpen", isOpen);
+        }
+    }
+}
